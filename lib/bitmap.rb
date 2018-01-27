eeba80daf20b4baf2a37feb19fb22992b9a94b46
Craig Plummer <craig@craigplummer.uk>
2018-01-27 10:02:43 +0000

    Generating new Bitmap image

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,8 +1,13 @@
 class Bitmap
-  attr_reader :x, :y
+  attr_reader :x, :y, :image
 
   def initialize(x, y)
     @x = x
     @y = y
+    @image = build_bitmap_image
+  end
+
+  def build_bitmap_image
+    Array.new(x) { Array.new(y) {'O'} }
   end
 end
--- spec/unit/bitmap_spec.rb
+++ spec/unit/bitmap_spec.rb
@@ -3,4 +3,20 @@ require_relative '../../lib/bitmap'
 
 RSpec.describe Bitmap do
   subject { described_class.new(x, y) }
+
+  context 'with valid arguments' do
+    let(:x) { 3 }
+    let(:y) { 5 }
+    let(:expected_image) do
+      [
+        ['O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O']
+      ]
+    end
+
+    it 'should generate new bitmap image of correct size' do
+      expect(subject.image).to eq(expected_image)
+    end
+  end
 end
