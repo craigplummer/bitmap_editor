064445764eb85006f7d4e9b3af24d6c6400891a3
Craig Plummer <craig@craigplummer.uk>
2018-01-27 10:01:42 +0000

    Add tests and implementation for build bitmap command

--- spec/unit/commands/build_bitmap_command_spec.rb
+++ spec/unit/commands/build_bitmap_command_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe BuildBitmapCommand do
   subject { described_class.new(*args) }
 
   describe '#perform' do
-    let(:args) { ['7', '8'] }
+    context 'with valid arguments' do
+      let(:args) { ['7', '8'] }
 
-    it 'initializes a new bitmap object' do
-      expect(Bitmap).to receive(:new).with(7, 8).and_call_original
-      subject.perform
+      it 'initializes a new bitmap object' do
+        expect(Bitmap).to receive(:new).with(7, 8).and_call_original
+        subject.perform
+      end
+    end
+
+    context 'with invalid arguments' do
+      let(:args) { [251, -3] }
+
+      it 'raises an exception' do
+        expect{subject.perform}.to raise_error(RuntimeError, /Image size must be between 250 x 250 pixels/)
+      end
     end
   end
 end
